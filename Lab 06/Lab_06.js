be85e1d43bf672b223deebd827dc0c3f19431ad2
Muneeb <muneeb.official.686332@hotmail.com>
2022-11-07 18:58:28 +0500

    Lab 06 HTML CSS Done

---
+++ Lab 06/Lab_06.js
@@ -0,0 +1,5 @@
+let name = document.getElementById("name");
+let email = document.getElementById("email");
+let password = document.getElementById("password");
+let form = document.getElementById("form");
+
